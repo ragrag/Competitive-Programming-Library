512f9b9d08f5688af8cac88ade7075766f8cc0d5
ragrag <raggi144711@bue.edu.eg>
2018-02-10 17:12:58 +0200

    v1

--- Graph/Shortest Path/APSP/Floyd Warshall.cpp
+++ Graph/Shortest Path/APSP/Floyd Warshall.cpp
@@ -35,9 +35,9 @@ for (int i = 0; i < n; i++) {
 	}
 
 	for (int k = 0; k < n; k++) //k->i->j
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+	 for (int i = 0; i < n; i++)
+	  for (int j = 0; j < n; j++)
+	    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
 
 for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
