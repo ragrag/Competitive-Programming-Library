054a01c1f4364e4c05982aa229f6f68bfc0ee0f6
Raggi <ragy_design@yahoo.com>
2018-03-19 00:48:34 +0200

    v1

--- Problems(UVA_CF)/UVA/821_Page Hopping.cpp
+++ Problems(UVA_CF)/UVA/821_Page Hopping.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
 #define INF 1000000000
 #define fast ios_base::sync_with_stdio(false)
 #define pb push_back
@@ -11,107 +15,103 @@
 using namespace std;
 typedef long long ll;
 typedef vector <int> vi;
-typedef pair<int,int> ii;
+typedef pair<int, int> ii;
 
 int adj[400][400];
 int p[400][400];
 
 void printPath(int i, int j) {
-if (i != j) printPath(i, p[i][j]);
-cout<<j<<" ";
+	if (i != j) printPath(i, p[i][j]);
+	cout << j << " ";
 }
 
 int fact(int n)
 {
-    if (n <= 1)
-        return 1;
-    return n*fact(n-1);
+	if (n <= 1)
+		return 1;
+	return n*fact(n - 1);
 }
 
 int nPr(int n, int r)
 {
-    return fact(n)/fact(n-r);
+	return fact(n) / fact(n - r);
 }
 
 int main()
 {
 
+	vector <string> anss;
+	int cas = 0;
 
-int cas =0;
-bool casee = false;
-bool endd = false;
-
-while(true)
-{
-
-bool ar[100] = {0};
-
-for (int i = 1; i <=100; i++) {
-    for (int j = 1; j <= 100; j++)
-	{
-      adj[i][j] = INF;
-	  p[i][j] =i;
-	}
-    adj[i][i] = 0;
-  }
-
-  int from, to;
-    cin >> from >> to;
-if(from ==0 && to ==0 )
-    break;
-    while(1)
+	while (true)
 	{
 
+		int ar[100] = { 0 };
 
-adj[from][to] = 1;
-ar[from] = 1;
-ar[to] = 1;
-	if(from ==0 && to==0 )
-{
-    break;
-}
-cin>>from>>to;
+		for (int i = 1; i <= 110; i++) {
+			for (int j = 1; j <= 110; j++)
+			{
+				adj[i][j] = INF;
+				p[i][j] = i;
+			}
+			adj[i][i] = 0;
+		}
 
+		int from, to;
+		cin >> from >> to;
+		if (from == 0 && to == 0)
+			break;
+		while (1)
+		{
+
+
+			adj[from][to] = 1;
+			ar[from] = 1;
+			ar[to] = 1;
+
+			cin >> from >> to;
+			if (from == 0 && to == 0)
+			{
+				break;
+			}
+		}
+
+		for (int k = 1; k <= 110; k++) //k->i->j
+			for (int i = 1; i <= 110; i++)
+				for (int j = 1; j <= 110; j++)
+					if (adj[i][k] + adj[k][j] < adj[i][j]) {
+						adj[i][j] = adj[i][k] + adj[k][j];
+						p[i][j] = p[k][j];
+					}
+
+
+
+
+		long double c = 0;
+		long double cc = 0;
+		for (int i = 1;i <= 105;i++)
+
+		{
+			for (int j = 1;j <= 105;j++)
+			{
+				if (i == j)
+					continue;
+				if (i != j && adj[i][j] != INF)
+				{
+					c += adj[i][j];
+					cc++;
+				}
+
+			}
+		}
+	
+		float ans = c / cc;
+		cout.precision(3);
+
+		cout << fixed << "Case " << ++cas << ": average length between pages = " << ans << " clicks" << endl;
 	}
 
-	for (int k = 1; k <= 100; k++) //k->i->j
-	 for (int i = 1; i <= 100; i++)
-	  for (int j = 1; j <=100; j++)
-	    if (adj[i][k] + adj[k][j] < adj[i][j]) {
-	     adj[i][j] = adj[i][k] + adj[k][j];
-	     p[i][j] = p[k][j];
-       }
-
-
-
-
-float c=0;
-float cc=0;
-for(int i=1;i<=100;i++)
-
-{
-for(int j=1;j<=100;j++)
-{
-    if(ar[i] && ar[j])
-    {
-
-
-    if(i==j)
-        continue;
-    c+=adj[i][j];
-    cc++;
-    }
-
+	return 0;
 }
-}
-
-float ans = c/cc;
- cout.precision(3);
-cout<<fixed<<"Case "<<++cas<<": average length between pages = "<<ans<<" clicks"<<endl;
-
-}
-
-        return 0;
-    }
 
 
